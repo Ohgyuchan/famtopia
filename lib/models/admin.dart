8560669e91a88c669d5567d7204ad5fc3f612193
Ohgyuchan <gyu3637@gmail.com>
2021-06-12 02:46:22 +0900

    Create Admin Class

---
+++ lib/models/admin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Admin {
+  late final String uid;
+  late final boolean right;
+
+  AdminItem({
+    required this.uid,
+    required this.right,
+  });
+}
